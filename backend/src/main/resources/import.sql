c8420e937dc91a10240427db801a68f0103a6d15
Rodrigo Machado <rodrigo.cmachado@gmail.com>
2021-11-17 23:04:24 -0300

    Database seeding

---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO tb_category (name) VALUES ('Livros');
+INSERT INTO tb_category (name) VALUES ('Moda');
+INSERT INTO tb_category (name) VALUES ('Eletronicos');
+INSERT INTO tb_category (name) VALUES ('Celular');
